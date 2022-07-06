10c2d48ba364629de461c5bc4ab9fc9d175e441c
Joao Pereira <pereira.joao@estudante.ufjf.br>
2022-07-06 19:38:43 -0300

    Aula07

---
+++ Aula07/No.h
@@ -0,0 +1,13 @@
+class No
+{
+private:
+    int info; // valor armazenado no No
+    No *prox; // ponteiro para o proximo No
+public:
+    No(){};
+    ~No(){};
+    int getInfo() { return info; };
+    No *getProx() { return prox; };
+    void setInfo(int val) { info = val; };
+    void setProx(No *p) { prox = p; };
+};
