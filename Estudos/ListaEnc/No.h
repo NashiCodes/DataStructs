4b88f88c731123320944a213fb67472f8a6124f1
Joao Pereira <pereira.joao@estudante.ufjf.br>
2022-07-06 18:01:28 -0300

    Apenas salto de linha

--- Estudos/ListaEnc/No.h
+++ Estudos/ListaEnc/No.h
@@ -5,8 +5,9 @@ private:
     No *prox; // Ponteiro que aponta para o Proximo Nó
 
 public:
-    No(){};                                // Construtor
-    ~No(){};                               // Destrutor
+    No(){};  // Construtor
+    ~No(){}; // Destrutor
+
     int getinfo() { return info; };        // Pegar a informação do nó
     No *getprox() { return prox; };        // Pegar o proximo Ponteiro
     void setinfo(int val) { info = val; }; // Colocar a informação no nó
