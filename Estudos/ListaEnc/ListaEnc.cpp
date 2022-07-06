ae3e6c8962a6f280aaab535cf22510410807a3e7
Joao Pereira <pereira.joao@estudante.ufjf.br>
2022-07-06 18:23:49 -0300

    Comentarios

--- Estudos/ListaEnc/ListaEnc.cpp
+++ Estudos/ListaEnc/ListaEnc.cpp
@@ -25,6 +25,16 @@ void ListaEnc::insereInicio(int val)
 {
     No *p = new No(); // Cria um novo Nó para a lista
 
+    /* o comando No *p = new No() aloca um novo
+    nó de forma dinâmica, isto é, este novo nó só ́é criado
+    quando a operação ̃insereInicio(val) e executada.*/
+
+    /*Portanto, novos nós são adicionados à lista em tempo de execução 
+    de acordo com a demanda da aplicação.*/
+
+    /*Logo, em um instante de tempo, a memoria ocupada pela
+    lista e proporcional ao número de nós armazenados na mesma.*/
+
     p->setinfo(val);      // Inserindo a informação no Nó
     p->setprox(primeiro); // Preenche proximo
 
