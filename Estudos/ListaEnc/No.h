e7f766c7bc1588b9a31922e90b9209af94351ad1
Joao Pereira <pereira.joao@estudante.ufjf.br>
2022-07-06 17:57:49 -0300

    Funções Adicionadas

--- Estudos/ListaEnc/No.h
+++ Estudos/ListaEnc/No.h
@@ -5,10 +5,10 @@ private:
     No *prox; // Ponteiro que aponta para o Proximo Nó
 
 public:
-    No();                  // Construtor
-    ~No();                 // Destrutor
-    int getinfo();         // Pegar a informação do nó
-    No *getprox();         // Pegar o proximo Ponteiro
-    void setinfo(int val); // Colocar a informação no nó
-    void setprox(No *p);   // Setar o proximo Nó
+    No(){};                                // Construtor
+    ~No(){};                               // Destrutor
+    int getinfo() { return info; };        // Pegar a informação do nó
+    No *getprox() { return prox; };        // Pegar o proximo Ponteiro
+    void setinfo(int val) { info = val; }; // Colocar a informação no nó
+    void setprox(No *p) { prox = p; };     // Setar o proximo Nó
 };
