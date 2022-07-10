9f94f05c483f588462fd5d792dfff57e92d36f7a
Joao Pereira <pereira.joao@estudante.ufjf.br>
2022-07-09 22:25:54 -0300

    Classe da ListaDupla criado e comentado

---
+++ Estudos/ListaDupla/ListaD.h
@@ -0,0 +1,19 @@
+#include "NoDuplo.h"
+
+class ListaD
+{
+private:
+    NoDuplo *primeiro;
+    NoDuplo *ultimo;
+    int n;
+
+public:
+    ListaD();
+    ~ListaD();
+
+    void insereInicio(int val); //Insere no inicio da Lista
+    void insereFinal(int val);  //Insere no Final da Lista
+    void removeInicio();        //Remove o inicio da lista
+    void removeFinal();         //Remove o final da lista
+    bool busca(int val);        //Procura um valor na Lista
+};
