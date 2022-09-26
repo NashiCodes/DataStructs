12acef893bb3f04f462cd3b63719d630085d2bb1
Joao Pereira <pereira.joao@estudante.ufjf.br>
2022-09-26 15:05:35 -0300

    Atv02 concluida

--- Aula02/main.cpp
+++ Aula02/main.cpp
@@ -5,13 +5,16 @@
 
 using namespace std;
 
-void Atv01();
-int fatorial(int n);
 int numAleatorio(int a, int b);
+void Atv01();
+void Atv02();
+int fatorial(int n);
+void imprimeIntervalo(int a, int b, int inc);
 
 int main()
 {
     Atv01();
+    Atv02();
 }
 
 void Atv01()
@@ -25,7 +28,36 @@ void Atv01()
 
     fat = fatorial(n);
 
-    cout << "Fatorial de : " << n << " eh: " << fat<< endl;
+    cout << "Fatorial de : " << n << " eh: " << fat << endl
+         << endl;
+    
+}
+
+void Atv02()
+{
+    int a;
+    int b;
+    int inc;
+
+    a = numAleatorio(0, 499);
+    b = numAleatorio(500, 1000);
+    inc = numAleatorio(1, 10);
+
+    cout << "intervalo fechado entre: " << a << " e " << b << " com incremento de " << inc << endl
+         << endl;
+
+    imprimeIntervalo(a, b, inc);
+
+    cout << endl;
+}
+
+void imprimeIntervalo(int a, int b, int inc)
+{
+    if (a < b)
+    {
+        cout << a << " ";
+        imprimeIntervalo(a + inc, b, inc);
+    }
 }
 
 int fatorial(int n)
